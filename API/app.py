256561ed362b3a422e40c5eb041c949d84931512
Gabriel <gabomendua@gmail.com>
2026-06-30 10:30:02 -0600

    commit estructura 1, no me funciona :(

--- API/app.py
+++ API/app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 productos = {
-    "201":{"id": 201, "producto": "Teclado Mecánico RGB", "precio": 45.00, "stock": 12},
-    "202":{"id": 202, "producto": "Mouse inalámbrico", "precio": 18.50, "stock": 25},
-    "203":{"id": 203, "producto": "Monitor LED 24\"", "precio": 165.00, "stock": 8}
+    201:{"id": 201, "nombre": "Teclado Mecánico RGB", "precio": 45.00, "stock": 12},
+    202:{"id": 202, "nombre": "Mouse inalámbrico", "precio": 18.50, "stock": 25},
+    203:{"id": 203, "nombre": "Monitor LED 24\"", "precio": 165.00, "stock": 8}
 }
 
 @app.route("/")
@@ -14,12 +14,31 @@ def inicio():
 
 @app.get("/productos")
 def consultar_catalogo():
-    pass
+    return jsonify(list(productos.values()))
 
 @app.get("/productos/<int:id>")
 def consultar_un_producto():
-    pass
+    producto = productos.get(id)
+    if producto:
+        return jsonify(producto)
+    return jsonify({"Error":"Producto no encontrado"}), 400
 
 @app.post("/productos")
 def añadir_producto():
-    pass
+    nuevo_producto = request.get_json()
+    campos_requeridos = ("nombre", "precio", "stock")
+    # if not all(campo in producto for campo in campos_requeridos):
+    #     return jsonify({"Error": "Faltan campos requeridos"}), 400
+    if not nuevo_producto or not all(campo in nuevo_producto for campo in campos_requeridos):
+        return jsonify({"Error": "Faltan campos requeridos"}), 400
+    
+    id_producto = max(productos.keys(), default=200) + 1
+    if id_producto in productos:
+        return jsonify({"Error": "El producto ya existe"}), 400
+    
+    nuevo_producto["id"] = id_producto
+    productos[id_producto] = nuevo_producto
+    return jsonify(nuevo_producto), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
